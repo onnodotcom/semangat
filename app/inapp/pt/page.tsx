cd36709c885d8f2ffddf798af613c1900073bb0d
onnodotcom <onnodotcom@gmail.com>
2023-09-14 11:29:20 +0700

    ok

--- app/inapp/pt/page.tsx
+++ app/inapp/pt/page.tsx
@@ -6,24 +6,6 @@ import ZPT from "./ZPT";
 export default function Page() {
   return (
     <>
-      <div className="text-xs mb-2">
-        <BreadcrumbAntd
-          items={[
-            {
-              title: <a href="">utama</a>,
-            },
-            {
-              title: <a href="">menu level 1</a>,
-            },
-            {
-              title: <a href="">menu level 3</a>,
-            },
-            {
-              title: "PT",
-            },
-          ]}
-        />
-      </div>
       <div className="text-xs">
         <CardAntd>
           <ZPT />
